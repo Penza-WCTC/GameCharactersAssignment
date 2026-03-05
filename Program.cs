65c5b414698836a6016815b12a73f8e70398a36f
KPenza <kpenza1@my.wctc.edu>
2026-03-05 15:00:14 -0600

    Deserialize the three Jsons from file

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using NLog;
+using System.Text.Json;
+using NLog;
 string path = Directory.GetCurrentDirectory() + "//nlog.config";
 
 // create instance of Logger
@@ -6,6 +7,14 @@ var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassL
 
 logger.Info("Program started");
 
-Console.WriteLine("Hello World!");
+// deserialize mario json from file into List<Mario>
+string marioFileName = "mario.json";
+List<Mario> marios = JsonSerializer.Deserialize<List<Mario>>(File.ReadAllText(marioFileName))!;
+
+string dkFileName = "dk.json";
+List<DonkeyKong> donkeyKongs = JsonSerializer.Deserialize<List<DonkeyKong>>(File.ReadAllText(dkFileName))!;
+
+string sf2FileName = "sf2.json";
+List<StreetFighter2> streetFighter2s = JsonSerializer.Deserialize<List<StreetFighter2>>(File.ReadAllText(sf2FileName))!;
 
 logger.Info("Program ended");
