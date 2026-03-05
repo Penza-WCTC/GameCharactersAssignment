675bf9557cca4682847d0e44bcbaee2dec5427cc
KPenza <kpenza1@my.wctc.edu>
2026-03-05 15:18:15 -0600

    initial menu setup (list selector)

--- Program.cs
+++ Program.cs
@@ -7,14 +7,84 @@ var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassL
 
 logger.Info("Program started");
 
-// deserialize mario json from file into List<Mario>
-string marioFileName = "mario.json";
-List<Mario> marios = JsonSerializer.Deserialize<List<Mario>>(File.ReadAllText(marioFileName))!;
+//Make sure that 1 list is selected.
+string listSelected;
 
-string dkFileName = "dk.json";
-List<DonkeyKong> donkeyKongs = JsonSerializer.Deserialize<List<DonkeyKong>>(File.ReadAllText(dkFileName))!;
+while (true)
+{
 
-string sf2FileName = "sf2.json";
-List<StreetFighter2> streetFighter2s = JsonSerializer.Deserialize<List<StreetFighter2>>(File.ReadAllText(sf2FileName))!;
+    Console.WriteLine("1) Access Mario");
+    Console.WriteLine("2) Access Donkey Kong");
+    Console.WriteLine("3) Access Street Fighter 2");
+
+    string? characterChoice = Console.ReadLine();
+    logger.Info("User choice: {Choice}", characterChoice);
+
+
+    if (characterChoice == "1")
+    {
+        string marioFileName = "mario.json";
+        List<Mario> marios = JsonSerializer.Deserialize<List<Mario>>(File.ReadAllText(marioFileName))!;
+        listSelected = "Mario";
+        break;
+    }
+
+    else if (characterChoice == "2")
+    {
+        string dkFileName = "dk.json";
+        List<DonkeyKong> donkeyKongs = JsonSerializer.Deserialize<List<DonkeyKong>>(File.ReadAllText(dkFileName))!;
+        listSelected = "DonkeyKong";
+        break;
+    }
+
+    else if (characterChoice == "3")
+    {
+        string sf2FileName = "sf2.json";
+        List<StreetFighter2> streetFighter2s = JsonSerializer.Deserialize<List<StreetFighter2>>(File.ReadAllText(sf2FileName))!;
+        listSelected = "StreetFighter2";
+        break;
+    }
+
+    else
+    {
+        Console.WriteLine("Select from one of the provided lists");
+    }
+}
+
+do
+{
+    // display choices to user
+    Console.WriteLine("1) Display the "+ listSelected +" Characters");
+    Console.WriteLine("2) Add a "+listSelected+" Character");
+    Console.WriteLine("3) Remove a "+listSelected+" Character");
+    Console.WriteLine("Enter to quit");
+
+    // input selection
+    string? choice = Console.ReadLine();
+    logger.Info("User choice: {Choice}", choice);
+
+    if (choice == "1")
+    {
+        // Display Mario Characters
+    }
+    else if (choice == "2")
+    {
+        // Add Mario Character
+    }
+    else if (choice == "3")
+    {
+        // Remove Mario Character
+    }
+    else if (string.IsNullOrEmpty(choice))
+    {
+        break;
+    }
+    else
+    {
+        logger.Info("Invalid choice");
+    }
+} while (true);
+
+logger.Info("Program ended");
 
 logger.Info("Program ended");
